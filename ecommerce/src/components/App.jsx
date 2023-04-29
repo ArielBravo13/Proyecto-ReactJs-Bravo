f4b58faaccabcf8249e61246314798a1807df1ad
ArielBravo13 <110268130+ArielBravo13@users.noreply.github.com>
2023-04-29 16:50:09 -0300

    Pre-Entrega 2
    Pre Entrega 2 React Js

---
+++ ecommerce/src/components/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+export const App = () => {
+
+  const valor = 5
+
+  return (
+    <div className="App">
+      
+    </div>
+ 
+  );
+}
---
+++ ecommerce/src/components/Categorias/Categorias.jsx
@@ -0,0 +1,11 @@
+export const Categorias = () => {
+    return (
+        <ul>
+            <li className='categoria-item'><a href="#">Zapatillas</a></li>
+            <li className='categoria-item'><a href="#">Remeras</a></li>
+            <li className='categoria-item'><a href="#">Camperas</a></li>            
+        </ul>
+    )
+};
+
+export default Categorias
